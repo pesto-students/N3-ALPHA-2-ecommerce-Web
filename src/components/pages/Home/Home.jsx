cb6965952374c5fa0e21fcb49485579a6d1b7306
Rohit <rohitdeveloper9@gmail.com>
2021-07-01 21:36:38 +0530

    feature/errorBoundryPwa : loader in route

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,15 +2,28 @@ import React, { Fragment } from 'react';
 import Banner from './Banner';
 import NewArrivals from './NewArrivals';
 import { getSessionStorage } from '../../../helper/Utils';
+import FullPageLoader from '../../shared/Loaders/FullPageLoader';
 
 const Home = () => {
     const newArrivals = getSessionStorage('arrivals') || [];
     const recomended = getSessionStorage('recomended') || [];
     return (
         <Fragment>
-            <Banner />
-            <NewArrivals heading="New Arrivals" selectedProd={newArrivals} />
-            <NewArrivals heading="Recommended" selectedProd={recomended} />
+            {recomended.length > 0 ? (
+                <Fragment>
+                    <Banner />
+                    <NewArrivals
+                        heading="New Arrivals"
+                        selectedProd={newArrivals}
+                    />
+                    <NewArrivals
+                        heading="Recommended"
+                        selectedProd={recomended}
+                    />
+                </Fragment>
+            ) : (
+                <FullPageLoader />
+            )}
         </Fragment>
     );
 };
