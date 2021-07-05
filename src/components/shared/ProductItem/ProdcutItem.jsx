fc7c4d16e3db2f05f95e426efadbfbfbb74efb66
Rohit <rohitdeveloper9@gmail.com>
2021-07-05 23:04:07 +0530

    image loader fix

--- src/components/shared/ProductItem/ProdcutItem.jsx
+++ src/components/shared/ProductItem/ProdcutItem.jsx
@@ -1,39 +1,62 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import './productItem.scss';
 import { CartContext } from '../Contexts/CartContext';
 
 export default function ProductItem(props) {
+    const [showImgLoader, setImgLoader] = useState(true);
     const { name, img, price, id } = props;
     const { cartItems, addProduct, increase } = useContext(CartContext);
     const isInCart = (product) => {
         return !!cartItems.find((item) => item.id === product.id);
     };
 
+    if (showImgLoader) {
+        const objImg = new Image();
+        objImg.src = props.img;
+        objImg.onload = () => {
+            setImgLoader(false);
+        };
+    }
+
     return (
-        <div className="product-item">
-            <Link to={`/products/${id}`} className="product-item-link">
-                <img src={img} className="product-item-img" alt={name} />
-                <div className="product-content">
-                    <h1 className="product-item-heading">{name}</h1>
-                    <p className="product-item-price">&#8377; {price}</p>
-                </div>
-            </Link>
-            {isInCart(props) ? (
-                <button
-                    className="product-item-cart"
-                    onClick={() => increase(props)}
-                >
-                    Add More
-                </button>
-            ) : (
-                <button
-                    className="product-item-cart"
-                    onClick={() => addProduct(props)}
-                >
-                    Add to cart
-                </button>
-            )}
-        </div>
+        <Fragment>
+            <div className="product-item">
+                <Link to={`/products/${id}`} className="product-item-link">
+                    {showImgLoader ? (
+                        <img
+                            src="/assets/loader.svg"
+                            className="product-item-loader"
+                            alt="Loader"
+                        />
+                    ) : (
+                        <img
+                            src={img}
+                            className="product-item-img"
+                            alt={name}
+                        />
+                    )}
+                    <div className="product-content">
+                        <h1 className="product-item-heading">{name}</h1>
+                        <p className="product-item-price">&#8377; {price}</p>
+                    </div>
+                </Link>
+                {isInCart(props) ? (
+                    <button
+                        className="product-item-cart"
+                        onClick={() => increase(props)}
+                    >
+                        Add More
+                    </button>
+                ) : (
+                    <button
+                        className="product-item-cart"
+                        onClick={() => addProduct(props)}
+                    >
+                        Add to cart
+                    </button>
+                )}
+            </div>
+        </Fragment>
     );
 }
